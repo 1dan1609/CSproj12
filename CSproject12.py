99af5c3c0c95ef75f6f28e6087707a20294602e1
Vandan <vandanagrawal1203@gmail.com>
2019-11-04 18:02:21 +0530

    Update CSproject12.py

--- CSproject12.py
+++ CSproject12.py
@@ -12,20 +12,20 @@ mydb=sql.connect(host="localhost",user="root",passwd="1609",db="Medstore")
 cursor=mydb.cursor()
 mydb.close()
 
-def click_login(uid,passwd,page,wrong_c):
+def click_login(uid,passwd,caller,wrong_c):
     mydb.connect()
     cursor.execute(f"select passwd from staff where username='{uid}'")
-    epassw=cursor.fetchall()[0][0] #Actual password
-    mydb.close()
-    if epassw==passwd:
-        page.destroy()
-        MainMenu()   
+    output=cursor.fetchall()
+    if cursor.rowcount==1:
+        epassw=output[0][0]
+        mydb.close()
+        if epassw==passwd:
+            caller.destroy()
+            MainMenu()   
+        else:
+            wrong_c.grid(row=0,column=2)
     else:
         wrong_c.grid(row=0,column=2)
-        
-def click_signup(page):
-    page.destroy()
-    SignUp()
     
 #%% Login page        
 def LoginPage():
@@ -40,7 +40,7 @@ def LoginPage():
     Entry(login, textvariable=username,width=50).grid(row=0,column=1)
     Entry(login, textvariable=passwd,width=50,show='*').grid(row=1,column=1)
     Button(fm1, text="LOG IN", command=lambda: click_login(username.get(),passwd.get(),login,wrong_c)).grid(row=0,column=0,sticky=W)
-    Button(fm1, text="SIGN UP", command=lambda: click_signup(login)).grid(row=0,column=1)
+    Button(fm1, text="SIGN UP", command=lambda: signup(login)).grid(row=0,column=1)
     login.mainloop()
 
 #%%
@@ -56,16 +56,17 @@ def MainMenu():
     fr3.grid(row=1,column=0,sticky="nsew")
     fr4.grid(row=1,column=1,sticky="nsew")
     Button(fr1,text="Billing").pack(fill='x')
-    Button(fr2,text="Stock").pack(fill='x')
+    Button(fr2,text="Stock",command=lambda: stock(mnmnu)).pack(fill='x')
     Button(fr3,text="Customers").pack(fill='x')
     Button(fr4,text="History").pack(fill='x')
-
     mnmnu.mainloop()
+    
 #%%
-def SignUp():
+def SignUp(caller):
+    caller.destroy()
     newacc=Tk()
     newacc.title("Sign up")
-    newacc.geometry("500x310")
+    newacc.geometry("500x370")
     Label(newacc,text="Enter employee id*: ").grid(row=0,column=0,sticky=W)
     Label(newacc,text="Enter initial passcode*: ").grid(row=1,column=0,sticky=W)
     Label(newacc,text="Enter your name*: ").grid(row=2,column=0,sticky=W)
@@ -107,12 +108,12 @@ def SignUp():
                                                                       newacc)).grid(row=11,column=0,sticky=W)
     newacc.mainloop()
     
-def click_createacc(eid,name,ipass,uname,passwd,repass,mobno,dob,gender,page):
+def click_createacc(eid,name,ipass,uname,passwd,repass,mobno,dob,gender,caller):
     mydb.connect()
     cursor.execute(f"select ipass,name from staff where eid='{eid}'")
     check = cursor.fetchall()
-    print(check)
-    error=Frame(page)
+    print(check) #remove later
+    error=Frame(caller)
     error.grid(row=11,column=1,ipadx=83,sticky=W)
     if cursor.rowcount == 0:
         Label(error, text="Please contact your employer for a valid ID.", fg="red").pack(fill='x',side=LEFT)
@@ -141,7 +142,76 @@ def click_createacc(eid,name,ipass,uname,passwd,repass,mobno,dob,gender,page):
                        where eid='{eid}'")
         mydb.commit()
         mydb.close()
-        page.destroy()
+        caller.destroy()
         LoginPage()
+        
+#%% 
+def stock(caller):
+    caller.destroy()
+    inventory=Tk()
+    additems=Frame(inventory)
+    additems.grid(row=1,column=0,columnspan=3)
+    Label(inventory,text="Add to stock:-\n").grid(row=0,column=0,sticky=W)
+    Label(additems,text="Seriel no.*:").grid(row=0,column=0,sticky=W)
+    Label(additems,text="Batch code*:").grid(row=0,column=2,sticky=W)
+    Label(additems,text="Manufacturer:").grid(row=1,column=0,sticky=W)
+    Label(additems,text=" Supplier:").grid(row=1,column=2,sticky=W)
+    Label(additems,text="Product name*:").grid(row=2,column=0,sticky=W)
+    Label(additems,text=" Quantity:").grid(row=2,column=2,sticky=W)
+    Label(additems,text=" Rate*:").grid(row=2,column=4,sticky=W)
+    Label(additems,text="Expiry:").grid(row=3,column=0,sticky=W)
+    Label(inventory,text="Fields marked '*' are important.").grid(row=2,column=0,sticky=W)
+    seriel,batchcode,manu,supp,pname,qty,rate,exp=StringVar(),StringVar(),StringVar(),StringVar(),StringVar(),StringVar(),StringVar(),StringVar()
+    e1=Entry(additems,textvariable=seriel,width=20)
+    e1.grid(row=0,column=1,sticky=W)
+    e2=Entry(additems,textvariable=batchcode,width=20)
+    e2.grid(row=0,column=3)
+    e3=Entry(additems,textvariable=manu,width=30)
+    e3.grid(row=1,column=1,sticky=W)
+    e4=Entry(additems,textvariable=supp,width=30)
+    e4.grid(row=1,column=3,columnspan=3,sticky=W)
+    e5=Entry(additems,textvariable=pname,width=30)
+    e5.grid(row=2,column=1,sticky=W)
+    e6=Entry(additems,textvariable=qty,width=10)
+    e6.grid(row=2,column=3,sticky=W)
+    e7=Entry(additems,textvariable=rate,width=12)
+    e7.grid(row=2,column=5,sticky=W)
+    e8=Entry(additems,textvariable=exp,width=20)
+    e8.grid(row=3,column=1,sticky=W)
+    entries=[e1,e2,e3,e4,e5,e6,e7,e8]
+    Button(inventory,text="Add entry",command=lambda: click_entry("new",inventory,entries,
+                                                                  seriel.get(),
+                                                                  manu.get(),
+                                                                  supp.get(),
+                                                                  pname.get(),
+                                                                  qty.get(),
+                                                                  rate.get(),
+                                                                  exp.get(),
+                                                                  batchcode.get())).grid(row=3,column=0,sticky=W)
+    inventory.mainloop()
+    
+def click_entry(mode,caller,entries,serielno,manufacturer,supplier,itemname,qty,rate,expiry,batchcode):
+    error=Frame(caller)
+    error.grid(row=3,column=1,sticky=W,ipadx=60)
+    if serielno=='':
+        Label(error,text="Seriel number is necessary.",fg='red').grid(row=0,column=0,sticky=W)
+    elif itemname=='':
+        Label(error,text="Product name is necessary.",fg='red').grid(row=0,column=0,sticky=W)
+    elif rate=='':
+        Label(error,text="Rate is necessary",fg='red').grid(row=0,column=0,sticky=W)
+    elif batchcode=='':
+        Label(error,text="Batch code is necessary",fg='red').grid(row=0,column=0,sticky=W)
+    else: 
+        manufacturer='null' if manufacturer=='' else "'"+manufacturer+"'"
+        supplier='null' if supplier=='' else "'"+supplier+"'"
+        qty=0 if qty=='' else qty
+        expiry='null' if expiry=='' else "'"+expiry+"'"
+        mydb.connect()
+        cursor.execute(f"insert into stock values('{serielno}',{supplier},{manufacturer},\
+                                                  '{itemname}',{qty},{rate},{expiry},'{batchcode}')")
+        mydb.commit()
+        for i in entries:
+            i.delete(0,100)
+        mydb.close()
 #%%
 LoginPage()
